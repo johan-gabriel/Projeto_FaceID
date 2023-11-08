79e3971426cfd25e7d8f8997e633acce4c355801
JmonsterK <johangabriel1@gmail.com>
2023-11-08 13:55:46 -0300

    Atualização Ethernet

---
+++ FID_Ethernet_10E/src/main.c
@@ -0,0 +1,596 @@
+﻿/*
+    Desenvolvimento da Placa de Controle de Catraca (PCC) para sistema Facial;
+
+    Inicio do desenvolvimento - 20/07/2022
+    Fim da da primeira versão - 17/08/2022
+
+    Equipe de desenvolvimento:
+    Firmware - JOHAN SILVA / BRUNO NUNES
+    Hardware - BRUNO RIBEIRO
+
+    Sistema desenvolvido para a empresa PiscoIDC, para o equipamento FACEID - CONTROLID
+    para controle de catracas de controle de acesso.
+
+
+    Versão: 1.0E (Com atualização para leitura e escrita no catraca.)
+    Funcionando!
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pico/stdlib.h>
+#include "CH9121_Test.h"
+
+
+#define S_LIBERADO 25//Led interno da rasp
+#define S_ESQ 10 // Pictograma verde
+#define S_DIR 11 // Pictograma verde
+#define S_BLOCK 7 // Pictograma vermelho
+
+#define S_BUZZ 6 // BUZER
+#define SOL1 15 // SOLENOIDE    
+#define SOL2 27 //SOLENOIDE
+#define SENSOR2 20 //SENSOR
+#define FACE_ID 26 //Sinal de liberardo externo;
+#define SENSOR1 21 //SENSOR
+
+void loop(void);
+int liberado(int var);
+int bloqueado(int blok);
+void RX_TX(int mensagem);
+void Regula_Tensao();
+
+
+int contador = 0;
+int libBlok = 0;
+int blokLib = 0;
+int giroGirado = 0;
+int autoriza = 0;
+int tempo = 0;
+int auxTemp = 3000; // tempo reserva; Para conseguir aumentar o tempo de liberação
+int inTemp = 0;
+
+int saida = 0; // sinaliza a mensagem que é enviada
+//int indice = 0;
+char comand;// comando do catraca
+char versFirmware[10] = "1.0E";
+int hash,q,valor =0; // vars responsáveis por ler e interpretar a mensagem;
+bool hora_ante = true;
+int y = 0; 
+int sentido = 0;
+
+
+bool sinalMisto = false;
+bool stateLibera = false;
+bool lastStateButton = false;
+
+// Variáveis dos comandos possíveis / comparativos
+int main()
+{
+    stdio_init_all();
+    Pico_ETH_CH9121_test();
+    //Exemplo de saida
+    gpio_init(S_LIBERADO);
+    gpio_set_dir(S_LIBERADO, GPIO_OUT);
+
+    gpio_init(S_DIR);
+    gpio_set_dir(S_DIR, GPIO_OUT);
+
+    gpio_init(S_ESQ);
+    gpio_set_dir(S_ESQ, GPIO_OUT);
+
+    gpio_init(S_BLOCK);
+    gpio_set_dir(S_BLOCK, GPIO_OUT);
+    gpio_pull_up(S_BLOCK);
+
+    gpio_init(S_BUZZ);
+    gpio_set_dir(S_BUZZ, GPIO_OUT);
+    gpio_pull_up(S_BUZZ);
+    
+
+    gpio_init(SOL2);
+    gpio_set_dir(SOL2, GPIO_OUT);
+    // Exemplo de saida
+    gpio_init(SOL1);
+    gpio_set_dir(SOL1, GPIO_OUT);
+
+    //Exemplo de entrada
+    gpio_init(FACE_ID);
+    gpio_set_dir(FACE_ID, GPIO_IN);
+    gpio_pull_up(FACE_ID);
+
+    //Exemplo de entrada
+    gpio_init(SENSOR2);
+    gpio_set_dir(SENSOR2, GPIO_IN);
+    gpio_pull_down(SENSOR2);
+
+    //Exemplo de entrada
+    gpio_init(SENSOR1);
+    gpio_set_dir(SENSOR1, GPIO_IN);
+    gpio_pull_down(SENSOR1);
+
+
+    while(1) loop();
+
+    return 0;
+}
+
+void loop(void){
+    
+    bool stateButton = !gpio_get(FACE_ID);
+    int liberar = liberado(contador);
+    int fimG = liberado(1);
+    
+    // ----------------------- Inicia a verificação ------------------------------
+    // Estagio liberado
+    if (stateButton == true)
+    {
+        autoriza = 1;
+        inTemp++;
+    }
+    if (inTemp >= 1500)
+    {
+        auxTemp = inTemp;
+      //  printf("%d\n",auxTemp);
+    }
+    //Tempo de liberação em ms
+    if(tempo == auxTemp){
+        autoriza = 0;
+        tempo = 0;
+        inTemp = 0;
+    }
+   // printf("|a%d|",auxTemp);
+    //printf(" %d \n",inTemp);
+   // printf("|t%d|",tempo);
+    if (autoriza == 1)
+    { 
+        tempo++;
+       // printf("Tempo %d\n", tempo);
+        contador = 1;
+        liberar = liberado(contador);
+        liberado(contador);
+        
+        // Bloqueia giro > 1
+        if (liberar == 3 && libBlok == 0) {
+            libBlok = 1;
+            giroGirado = 1;
+        }  
+        if (libBlok == 1)
+        {
+            if(liberar == 1){
+                libBlok = 2;
+                giroGirado = 1;
+                sentido = 3;
+            }
+            if (liberar == 2)
+            {
+                libBlok = 2;
+                giroGirado =1;
+                sentido = 4;
+                
+            }
+            
+        }  
+        if (libBlok == 2){
+            if (liberar == 0)
+            {
+                libBlok = 3;
+                giroGirado = 1;  
+            } 
+        }
+        if (libBlok == 3){
+            //autoriza = 0;
+            bloqueado(0);
+            giroGirado=0;
+            gpio_put(S_ESQ, 0);
+           // gpio_put(S_DIR, 1);
+            autoriza = 0;
+            tempo =0;
+            RX_TX(sentido);
+            
+        }
+        //gpio_put(S_LIBERADO, 1);
+        
+    }
+    // bloqueado no stand by
+    if (autoriza == 0 && giroGirado == 0){
+        gpio_put(S_LIBERADO, 0);
+        gpio_put(S_ESQ, 0);
+        //gpio_put(S_DIR, 1);
+        contador = 0;
+        if (contador == 0){
+            bloqueado(contador);
+            libBlok = 0;
+            blokLib = 0;
+        }   
+    }
+   
+    if (autoriza == 1 && sinalMisto == false){
+            gpio_put(S_LIBERADO, 1);
+            // meio do giro no liberado, mas travou
+        if (autoriza == 0 && giroGirado == 1 && fimG >= 1){
+            gpio_put(S_LIBERADO, 0);
+            // Acende o Pictograma verde enquanto não houver terminado o giro ou parar a liberação
+            gpio_put(S_ESQ, 1);
+            gpio_put(S_DIR, 0);
+            if (giroGirado == 1 && (fimG == 1|| fimG == 2)){
+                blokLib = 1;      
+            }
+        }
+        if (blokLib == 1 && fimG ==0)
+        {   gpio_put(S_LIBERADO,0);
+            bloqueado(0);
+            giroGirado =0;
+            gpio_put(S_ESQ, 0);
+            //gpio_put(S_DIR, 1);
+            autoriza = 0; 
+        }
+        if (contador == 0 && giroGirado == 1){
+            gpio_put(S_LIBERADO,0);
+            // Acende o Pictograma verde enquanto não houver terminado o giro ou parar a liberação
+            gpio_put(S_ESQ, 1);
+            gpio_put(S_DIR, 0);
+        }
+        if (contador == 1 && libBlok == 0)
+        {
+            gpio_put(S_LIBERADO,0);
+            // Acende o Pictograma verde enquanto não houver terminado o giro ou parar a liberação
+            gpio_put(S_ESQ, 1);
+            gpio_put(S_DIR, 0);
+            if (libBlok == 3 && contador == 1)
+            { 
+                gpio_put(S_LIBERADO,0);
+                gpio_put(S_ESQ, 0);
+               // gpio_put(S_DIR, 1);
+                autoriza = 0;
+            }
+            
+        }
+    }  
+    //--------------------------- FIM DO LOOP --------------------------------------
+// ----------------------------- LEITURA QUE VEM DO CATRACA ------------------------
+    while (uart_is_readable(UART_ID1))
+    {
+        comand = uart_getc(UART_ID1);
+        if(comand == '#')
+        {hash = 1; }
+        if(comand == 'S')
+        {   q = 5;
+            valor = valor + q;
+            q=0; 
+        }
+        if(comand == 'G')
+        {   q = 16;
+            valor = valor + q;
+            q=0; 
+        }
+        if(comand == 'B')
+        {   q = 12;
+            valor = valor + q;
+            q=0; 
+        }
+        if(comand == 'U')
+        {   q = 13;
+            valor = valor + q;
+            q=0; 
+        }
+        if(comand == 'Z')
+        {   q = 14;
+            valor = valor + q;
+            q=0; 
+        }
+        if(comand == 'R')
+        {   q = 10;
+            valor = valor + q;
+            q=0; 
+        }
+        if(comand == 'T')
+        {   q = 9;
+            valor = valor + q;
+            q=0; 
+        } 
+        if(comand == 'F')
+        {   q = 7;
+            valor = valor + q;
+            q=0; 
+        } 
+        if(comand == 'W')
+        {   q = 8;
+            valor = valor + q;
+            q=0; 
+        } 
+        if (comand == 'V')
+        {
+            hash =0;
+        }
+        if(comand == 'H')
+        {
+            q = 18;
+            valor = valor + q;
+            q = 0;
+        }
+        if(comand == 'h')
+        {
+            q = 1;
+            valor = valor + q;
+            q = 0;
+        }
+        if(comand == 'a')
+        {
+            q = 2;
+            valor = valor + q;
+            q = 0;
+        }
+        
+        if (uart_is_writable(UART_ID0))
+        {
+           // printf("Valor: %d", valor);
+            if(hash == 1){
+                if (valor)
+                {
+                    RX_TX(valor);
+                }
+            }
+            if(valor > 200){
+                valor = 0;
+            }
+            
+        }   
+    }
+//------------------------------ FIM DA LEITURA ------------------------------------   
+    
+}
+//Controla a liberação (Na praica o sinal de giro)
+int liberado(int var){
+    //gpio_put(S_LIBERADO, 1);
+    int aux = 0;
+    int gaveta = 0;
+    if (var == 1 && aux == 0)
+    {   
+        // S1 = 0 && S2 = 0 
+        if (!gpio_get(SENSOR1) && !gpio_get(SENSOR2)){
+            aux = 0;
+           // printf("%d", aux);
+            return aux;}
+
+        // S1 = 1 and S2 = 0 
+        if (gpio_get(SENSOR1) && !gpio_get(SENSOR2)){
+            aux = 2;
+           // printf("S1 %d", aux);
+            return aux;}
+        //S1 = 0 and S2 = 1
+        if(!gpio_get(SENSOR1) && gpio_get(SENSOR2)){
+            aux = 1;
+           // printf("S2 %d", aux);
+            return aux;
+        }
+
+            // S1 = 1 and S2 = 1
+        if (gpio_get(SENSOR1) && gpio_get(SENSOR2)){
+            aux = 3; 
+            //printf("S1 + S2 %d", aux);
+            return aux;
+        }
+        //printf(" G: %d ",gaveta);
+    } 
+    
+}
+// controla bloqueio
+int bloqueado (int blok){
+
+      if(blok == 0) {
+      // Acionamento S1
+        if (gpio_get(SENSOR1))
+        {   
+            gpio_put(SOL2, 1);
+            // pictograma Vermelho
+            gpio_put(S_BLOCK,1);
+            gpio_put(S_BUZZ, 1);
+            Regula_Tensao();
+            blokLib = 0;
+            RX_TX(1);
+        }
+        else{
+            saida = 0;
+             gpio_put(SOL2, 0);
+             gpio_put(S_BLOCK,0);
+             gpio_put(S_BUZZ, 0);
+            //printf("ACIONOU O S1\n");
+            RX_TX(0);
+        }
+        // Acionamento S2
+         if (gpio_get(SENSOR2))
+        {
+            
+            gpio_put(SOL1, 1);
+            // pictograma Vermelho
+            gpio_put(S_BLOCK,1);
+            gpio_put(S_BUZZ, 1);
+            Regula_Tensao();
+            blokLib = 0;
+            RX_TX(1);
+            
+        }
+        else{
+            
+             gpio_put(SOL1, 0);
+             gpio_put(S_BLOCK,0);
+             gpio_put(S_BUZZ, 0);
+             //printf("ACIONOU O S1\n");
+             RX_TX(0);
+        }
+    }
+}
+// Controle de mensagem
+void RX_TX(int mensagem)
+{
+        switch (mensagem)
+        {
+            case 1:
+                uart_puts(UART_ID0, "RPAb");
+                sleep_ms(300);
+                mensagem = 0;
+                break;
+            case 2:
+                mensagem = 0;
+                break;
+            case 3:
+                uart_puts(UART_ID0, "RPAe");
+                sleep_ms(1);
+                mensagem = 0;
+                break;
+            case 4:
+                uart_puts(UART_ID0, "RPAs");
+                sleep_ms(1);
+                mensagem = 0;
+                break;
+            case 39:
+                gpio_put(S_BUZZ, 1);
+                Regula_Tensao();
+                uart_puts(UART_ID0,"BUZok");
+                sleep_ms(50);
+                gpio_put(S_BUZZ, 0);
+                valor = 0;
+                break;
+            case 21:
+                uart_puts(UART_ID0,"RSTok");
+                valor = 0;
+                hash=0;
+                break;
+            case 31:
+                uart_puts(UART_ID0,"GFW:");
+                printf(versFirmware);
+                valor = 0;
+                hash =0;
+                break;
+            case 29:
+                uart_puts(UART_ID0,"SSHhok");
+                hora_ante = true;
+                valor = 0;
+                hash = 0;
+                break;
+            case 30:
+                uart_puts(UART_ID0,"SSHaok");
+                hora_ante = false;
+                valor = 0;
+                hash = 0;
+                break;
+            default:
+                mensagem = 0;
+                break;
+        }
+        // faz a leitura e escreve o eco dela    
+          
+}
+// Regula a tensão de saida
+void Regula_Tensao(){
+     printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+            printf(NULL);
+}
---
+++ FID_Ethernet_12E_26_09_23/lib/CH9121/CH9121.c
@@ -0,0 +1,196 @@
+#include "CH9121.h"
+
+UCHAR CH9121_LOCAL_IP[4] = {169, 254, 88, 90};    // LOCAL IP
+UCHAR CH9121_GATEWAY[4] = {169, 254, 88, 1};      // GATEWAY
+UCHAR CH9121_SUBNET_MASK[4] = {255, 255, 255, 0}; // SUBNET MASK
+UCHAR CH9121_TARGET_IP[4] = {169, 254, 88, 17};   // TARGET_IP
+UWORD CH9121_PORT1 = 5000;                        // LOCAL PORT1
+UWORD CH9121_TARGET_PORT = 3000;                  // TARGET PORT
+UDOUBLE CH9121_BAUD_RATE = 115200;                // BAUD RATE
+
+UCHAR tx[8] = {0x57, 0xAB};
+
+/******************************************************************************
+function:	Send four bytes
+parameter:
+    data: parameter
+    command: command code
+Info:  Set mode, enable port, clear serial port, switch DHCP, switch port 2
+******************************************************************************/
+void CH9121_TX_4_bytes(UCHAR data, int command)
+{
+    for (int i = 2; i < 4; i++)
+    {
+        if (i == 2)
+            tx[i] = command;
+        else
+            tx[i] = data;
+    }
+    DEV_Delay_ms(10);
+    for (int o = 0; o < 4; o++)
+        uart_putc(UART_ID0, tx[o]);
+    DEV_Delay_ms(10);
+    for (int i = 2; i < 4; i++)
+        tx[i] = 0;
+}
+
+/******************************************************************************
+function:	Send five bytes
+parameter:
+    data: parameter
+    command: command code
+Info:  Set the local port and target port
+******************************************************************************/
+void CH9121_TX_5_bytes(UWORD data, int command)
+{
+    UCHAR Port[2];
+    Port[0] = data & 0xff;
+    Port[1] = data >> 8;
+    for (int i = 2; i < 5; i++)
+    {
+        if (i == 2)
+            tx[i] = command;
+        else
+            tx[i] = Port[i - 3];
+    }
+    DEV_Delay_ms(10);
+    for (int o = 0; o < 5; o++)
+        uart_putc(UART_ID0, tx[o]);
+    DEV_Delay_ms(10);
+    for (int i = 2; i < 5; i++)
+        tx[i] = 0;
+}
+/******************************************************************************
+function:	Send seven bytes
+parameter:
+    data: parameter
+    command: command code
+Info:  Set the IP address, subnet mask, gateway,
+******************************************************************************/
+void CH9121_TX_7_bytes(UCHAR data[], int command)
+{
+    for (int i = 2; i < 7; i++)
+    {
+        if (i == 2)
+            tx[i] = command;
+        else
+            tx[i] = data[i - 3];
+    }
+    DEV_Delay_ms(10);
+    for (int o = 0; o < 7; o++)
+        uart_putc(UART_ID0, tx[o]);
+    DEV_Delay_ms(10);
+    for (int i = 2; i < 7; i++)
+        tx[i] = 0;
+}
+
+/******************************************************************************
+function:	CH9121_TX_BAUD
+parameter:
+    data: parameter
+    command: command code
+Info:  Set baud rate
+******************************************************************************/
+void CH9121_TX_BAUD(UDOUBLE data, int command)
+{
+    UCHAR Port[4];
+    Port[0] = (data & 0xff);
+    Port[1] = (data >> 8) & 0xff;
+    Port[2] = (data >> 16) & 0xff;
+    Port[3] = data >> 24;
+
+    for (int i = 2; i < 7; i++)
+    {
+        if (i == 2)
+            tx[i] = command;
+        else
+            tx[i] = Port[i - 3];
+    }
+    DEV_Delay_ms(10);
+    for (int o = 0; o < 7; o++)
+        uart_putc(UART_ID0, tx[o]);
+    DEV_Delay_ms(10);
+    for (int i = 2; i < 7; i++)
+        tx[i] = 0;
+}
+
+/******************************************************************************
+function:	CH9121_Eed
+parameter:
+Info:  Updating configuration Parameters
+******************************************************************************/
+void CH9121_Eed()
+{
+    tx[2] = 0x0d;
+    uart_puts(UART_ID0, tx);
+    DEV_Delay_ms(200);
+    tx[2] = 0x0e;
+    uart_puts(UART_ID0, tx);
+    DEV_Delay_ms(200);
+    tx[2] = 0x5e;
+    uart_puts(UART_ID0, tx);
+}
+
+/**
+ * delay x ms
+**/
+void DEV_Delay_ms(UDOUBLE xms)
+{
+    sleep_ms(xms);
+}
+
+void DEV_Delay_us(UDOUBLE xus)
+{
+    sleep_us(xus);
+}
+
+/******************************************************************************
+function:	CH9121_init
+parameter:
+Info:  Initialize CH9121
+******************************************************************************/
+void CH9121_init(void)
+{
+
+    uart_init(UART_ID0, BAUD_RATE);
+    uart_init(UART_ID1, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN0, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN0, GPIO_FUNC_UART);
+    gpio_set_function(UART_TX_PIN1, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN1, GPIO_FUNC_UART);
+
+    gpio_init(CFG_PIN);
+    gpio_init(RES_PIN);
+    gpio_set_dir(CFG_PIN, GPIO_OUT);
+    gpio_set_dir(RES_PIN, GPIO_OUT);
+    gpio_put(CFG_PIN, 1);
+    gpio_put(RES_PIN, 1);
+}
+
+/******************************************************************************
+function:	RX_TX
+parameter:
+Info:  Serial port 1 and serial port 2 receive and dispatch
+******************************************************************************/
+void RX_TX()
+{
+    while (1)
+    {
+        while (uart_is_readable(UART_ID0))
+        {
+            UBYTE ch0 = uart_getc(UART_ID0);
+            if (uart_is_writable(UART_ID0))
+            {
+                uart_putc(UART_ID0, ch0);
+            }
+        }
+        while (uart_is_readable(UART_ID1))
+        {
+            UBYTE ch1 = uart_getc(UART_ID1);
+            if (uart_is_writable(UART_ID1))
+            {
+                uart_putc(UART_ID1, ch1);
+            }
+        }
+    }
+}
